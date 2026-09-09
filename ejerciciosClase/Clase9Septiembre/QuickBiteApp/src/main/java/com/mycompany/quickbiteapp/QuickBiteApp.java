7765a49862da92be878acc428d09fa02eaa2d3f8
jcontreras609 <jcontreras609@unab.edu.co>
2026-09-09 17:55:07 -0500

    QuickBiteApp
    Todo lo que hice en clase

---
+++ ejerciciosClase/Clase9Septiembre/QuickBiteApp/src/main/java/com/mycompany/quickbiteapp/QuickBiteApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quickbiteapp;
+
+/**
+ *
+ * @author CSU22
+ */
+public class QuickBiteApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
